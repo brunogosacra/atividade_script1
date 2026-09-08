634f3b94a3a9ffc489fb3f9dee4ab7c1f3941829
brunogosacra <mcsgsbruno@gmail.com>
2026-09-08 16:06:21 -0300

    primeira atividade script

--- js01.js
+++ js01.js
@@ -2,22 +2,19 @@ function calcularIMC() {
     const pesoInput = document.getElementById('peso').value;
     const alturaInput = document.getElementById('altura').value;
 
-    
     const peso = parseFloat(pesoInput);
     const altura = parseFloat(alturaInput);
     const elementoResultado = document.getElementById('resultado');
 
-    
     if (!peso || !altura || peso <= 0 || altura <= 0) {
+        elementoResultado.style.display = "block";
         elementoResultado.innerHTML = "Por favor, digite valores válidos para peso e altura.";
         return;
     }
 
-    
     const imc = peso / (altura * altura);
     let classificacao = "";
 
-    
     if (imc < 18.5) {
         classificacao = "Magreza";
     } else if (imc >= 18.5 && imc <= 24.9) {
@@ -31,5 +28,6 @@ function calcularIMC() {
     }
 
     
-    elementoResultado.innerHTML = `IMC: ${imc.toFixed(2)} - Classificação: ${classificacao}`;
+    elementoResultado.style.display = "block";
+    elementoResultado.innerHTML = `IMC: ${imc.toFixed(2)}<br>Classificação:<br>${classificacao}`;
 }
