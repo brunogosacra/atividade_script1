d2b7cac0126f8c57b25e8bf157e0e53292b52e00
brunogosacra <mcsgsbruno@gmail.com>
2026-09-08 15:25:45 -0300

    primeira atvd de javascript

---
+++ js01.js
@@ -0,0 +1,35 @@
+function calcularIMC() {
+    const pesoInput = document.getElementById('peso').value;
+    const alturaInput = document.getElementById('altura').value;
+
+    
+    const peso = parseFloat(pesoInput);
+    const altura = parseFloat(alturaInput);
+    const elementoResultado = document.getElementById('resultado');
+
+    
+    if (!peso || !altura || peso <= 0 || altura <= 0) {
+        elementoResultado.innerHTML = "Por favor, digite valores válidos para peso e altura.";
+        return;
+    }
+
+    
+    const imc = peso / (altura * altura);
+    let classificacao = "";
+
+    
+    if (imc < 18.5) {
+        classificacao = "Magreza";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        classificacao = "Peso normal (adequado)";
+    } else if (imc >= 25.0 && imc <= 29.9) {
+        classificacao = "Sobrepeso";
+    } else if (imc >= 30.0 && imc <= 39.9) {
+        classificacao = "Obesidade";
+    } else {
+        classificacao = "Obesidade grave";
+    }
+
+    
+    elementoResultado.innerHTML = `IMC: ${imc.toFixed(2)} - Classificação: ${classificacao}`;
+}
